5908ff2297d87c403e64e9b05294257817ee252c
iqminiclip <andyruan@outlook.com>
2021-09-28 21:41:47 -0400

    Added comments for client and server files

--- src/client.py
+++ src/client.py
@@ -3,9 +3,11 @@ import threading
 
 class Client:
     def __init__(self):
+        # Go to create_connection function 
         self.create_connection()
 
     def create_connection(self):
+        # Initialize socket with IVP4 using TCP
         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         
         while 1:
@@ -20,18 +22,22 @@ class Client:
 
         self.username = input('Enter username --> ')
         self.s.send(self.username.encode())
-        
+        # Initialize two seperate threads per Client to send and recieve messages
+        # Initialize thread to recieve messages
         message_handler = threading.Thread(target=self.handle_messages,args=())
         message_handler.start()
 
+        # Initialize thread to send messages
         input_handler = threading.Thread(target=self.input_handler,args=())
         input_handler.start()
 
     def handle_messages(self):
+        # Print out any messages from the server
         while 1:
             print(self.s.recv(1204).decode())
 
     def input_handler(self):
+        # Take user input and send messages constantly.  Server will display message if non-null
         while 1:
             self.s.send((self.username+' - '+input()).encode())
 
--- src/server.py
+++ src/server.py
@@ -9,12 +9,11 @@ class Server:
         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         print(socket.gethostbyname(socket.gethostname()))
         host = socket.gethostbyname(socket.gethostname())
-        #host = '192.168.241.1'
-        #host = '192.168.1.29'
+
         port = int(input('Enter port to run the server on --> '))
-
+        # Client list
         self.clients = []
-
+        
         self.s.bind((host,port))
         self.s.listen(100)
     
@@ -24,36 +23,40 @@ class Server:
         self.username_lookup = {}
 
         while True:
+            # Check for new connections, where c = Client() and addr = address
             c, addr = self.s.accept()
-
             username = c.recv(1024).decode()
             
             print('New connection. Username: '+str(username))
+            # Broadcast to all clients on server via instance function
             self.broadcast('New person joined the room. Username: '+username)
-
+            # Append username to username list, and clients object to list
             self.username_lookup[c] = username
-
             self.clients.append(c)
-             
+            # Start a new thread to handle the client I/O
             threading.Thread(target=self.handle_client,args=(c,addr,)).start()
 
+    # Send message to all clients via broadcast
     def broadcast(self,msg):
         for connection in self.clients:
             connection.send(msg.encode())
 
+
     def handle_client(self,c,addr):
         while True:
             try:
+                # Recieve message from client
                 msg = c.recv(1024)
             except:
+                # When message cannot be recieved, shut down socket, remove from list
                 c.shutdown(socket.SHUT_RDWR)
                 self.clients.remove(c)
-                
+                # Broadcast message that user has left the room
                 print(str(self.username_lookup[c])+' left the room.')
                 self.broadcast(str(self.username_lookup[c])+' has left the room.')
 
                 break
-
+            # Broadcast to all clients other than sender if message is not blank
             if msg.decode() != '':
                 print('New message: '+str(msg.decode()))
                 for connection in self.clients:
